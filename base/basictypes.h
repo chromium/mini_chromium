chromium/mini_chromium

commit d40dd8c6b18ab2a4372a19706340d0ddac6aea4b

When using C++11, use static_assert to implement COMPILE_ASSERT, as in upstream https://codereview.chromium.org/77343005/ .

base/basictypes.h
@@ -36,6 +36,12 @@ inline To implicit_cast(const From& f) {
   return f;
 }
 
+#if __cplusplus >= 201103L
+ 
+#define COMPILE_ASSERT(expr, msg) static_assert(expr, #msg)
+
+#else
+
 template <bool>
 struct CompileAssert {
 };
@@ -51,5 +57,7 @@ inline Dest bit_cast(const Source& source) {
   memcpy(&dest, &source, sizeof(dest));
   return dest;
 }
+
+#endif
 
 #endif  // MINI_CHROMIUM_BASE_BASICTYPES_H_

commit ba9b15f1b6a7616ac0cb26069201a479e81845c3

Make bit_cast available in C++11 mode.
Its exclusion was an error introduced in d40dd8c6b18a.

base/basictypes.h
@@ -63,6 +63,8 @@ struct CompileAssert {
 #define COMPILE_ASSERT(expr, msg) \
     typedef CompileAssert<(bool(expr))> msg[bool(expr) ? 1 : -1] ALLOW_UNUSED
 
+#endif
+
 template <typename Dest, typename Source>
 inline Dest bit_cast(const Source& source) {
   COMPILE_ASSERT(sizeof(Dest) == sizeof(Source), sizes_must_be_equal);
@@ -71,8 +73,6 @@ inline Dest bit_cast(const Source& source) {
   memcpy(&dest, &source, sizeof(dest));
   return dest;
 }
-
-#endif
 
 template<typename T>
 inline void ignore_result(const T&) {