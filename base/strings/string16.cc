919fe9b586e9ef8d331d9dfbaf5de3e7e0a63c91
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-01-20 10:35:13 +0100

    Change non-Windows base::string16 to std::u16string
    This change mirrors the change of base::string16 to std::u16string in
    Chromium (https://crrev.com/841439) to mini_chromium.
    
    Bug: chromium:911896
    Change-Id: I0c59a29f49587305498ee933813ad690e46fa370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/mini_chromium/+/2637646
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- base/strings/string16.cc
+++ base/strings/string16.cc
@@ -12,60 +12,8 @@
 
 #include "base/strings/utf_string_conversions.h"
 
-namespace base {
-
-int c16memcmp(const char16* s1, const char16* s2, size_t n) {
-  while (n-- > 0) {
-    if (*s1 != *s2) {
-      return ((*s1 < *s2) ? -1 : 1);
-    }
-    ++s1;
-    ++s2;
-  }
-  return 0;
+std::ostream& operator<<(std::ostream& out, const std::u16string& str) {
+  return out << base::UTF16ToUTF8(str);
 }
-
-size_t c16len(const char16* s) {
-  const char16 *s_orig = s;
-  while (*s) {
-    ++s;
-  }
-  return s - s_orig;
-}
-
-const char16* c16memchr(const char16* s, char16 c, size_t n) {
-  while (n-- > 0) {
-    if (*s == c) {
-      return s;
-    }
-    ++s;
-  }
-  return 0;
-}
-
-char16* c16memmove(char16* s1, const char16* s2, size_t n) {
-  return reinterpret_cast<char16*>(memmove(s1, s2, n * sizeof(char16)));
-}
-
-char16* c16memcpy(char16* s1, const char16* s2, size_t n) {
-  return reinterpret_cast<char16*>(memcpy(s1, s2, n * sizeof(char16)));
-}
-
-char16* c16memset(char16* s, char16 c, size_t n) {
-  char16 *s_orig = s;
-  while (n-- > 0) {
-    *s = c;
-    ++s;
-  }
-  return s_orig;
-}
-
-std::ostream& operator<<(std::ostream& out, const string16& str) {
-  return out << UTF16ToUTF8(str);
-}
-
-}  // namespace base
-
-template class std::basic_string<base::char16, base::string16_char_traits>;
 
 #endif  // WCHAR_T_IS_UTF32
