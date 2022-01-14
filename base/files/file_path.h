c2bc7a2a195eb8940215d0b81767f022aa5ecfdd
Avi Drissman <avi@chromium.org>
2022-01-14 15:34:52 -0500

    Modernize code for C++17
    929dc059329b9e21073cfde90bdedce07162b671 removed macros obsoleted by
    C++17; this CL modernizes the code accordingly.
    
    Bug: 1286390, 1287045, 1283907
    Change-Id: I3299d00f19490c320349f2d497222ceb77b5c749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/mini_chromium/+/3388119
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- base/files/file_path.h
+++ base/files/file_path.h
@@ -107,7 +107,6 @@
 #include <iosfwd>
 #include <string>
 
-#include "base/compiler_specific.h"
 #include "build/build_config.h"
 
 // Windows-style drive letter support and pathname separator characters can be
@@ -182,21 +181,21 @@ class FilePath {
   // only contains one component, returns a FilePath identifying
   // kCurrentDirectory.  If this object already refers to the root directory,
   // returns a FilePath identifying the root directory.
-  FilePath DirName() const WARN_UNUSED_RESULT;
+  [[nodiscard]] FilePath DirName() const;
 
   // Returns a FilePath corresponding to the last path component of this
   // object, either a file or a directory.  If this object already refers to
   // the root directory, returns a FilePath identifying the root directory;
   // this is the only situation in which BaseName will return an absolute path.
-  FilePath BaseName() const WARN_UNUSED_RESULT;
+  [[nodiscard]] FilePath BaseName() const;
 
   // Returns the path's file extension. This does not have a special case for
   // common double extensions, so FinalExtension() of "foo.tar.gz" is simply
   // ".gz". If there is no extension, "" will be returned.
-  StringType FinalExtension() const WARN_UNUSED_RESULT;
+  [[nodiscard]] StringType FinalExtension() const;
 
   // Returns a FilePath with FinalExtension() removed.
-  FilePath RemoveFinalExtension() const WARN_UNUSED_RESULT;
+  [[nodiscard]] FilePath RemoveFinalExtension() const;
 
   // Returns a FilePath by appending a separator and the supplied path
   // component to this object's path.  Append takes care to avoid adding
@@ -204,8 +203,8 @@ class FilePath {
   // If this object's path is kCurrentDirectory, a new FilePath corresponding
   // only to |component| is returned.  |component| must be a relative path;
   // it is an error to pass an absolute path.
-  FilePath Append(const StringType& component) const WARN_UNUSED_RESULT;
-  FilePath Append(const FilePath& component) const WARN_UNUSED_RESULT;
+  [[nodiscard]] FilePath Append(const StringType& component) const;
+  [[nodiscard]] FilePath Append(const FilePath& component) const;
 
   // Returns true if this FilePath contains an absolute path.  On Windows, an
   // absolute path begins with either a drive letter specification followed by
